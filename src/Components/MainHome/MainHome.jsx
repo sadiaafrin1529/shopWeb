3756092883eed5fb259e0946093d41cb36150ce8
unknown <sadiaafrin1529@gmail.com>
2024-12-03 01:43:51 +0600

    navbarUpdate

--- src/Components/MainHome/MainHome.jsx
+++ src/Components/MainHome/MainHome.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import DesktopMenu from '../Header/DesktopMenu'
-import NavBar from '../Header/NavBar';
+
+
 
 const MainHome = () => {
   return (
     <div>
-      <NavBar/>
+      
     </div>
   );
 }
